1228b48c219bc3f9a76e8450a251a5dc0ae180df
yash114bansal <yash114bansal@gmail.com>
2023-08-25 19:44:14 +0530

    implemented cashAPI

--- CashAPI/urls.py
+++ CashAPI/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("notevalue/",views.NoteValue.as_view()),
-    path("get-otp/",views.SendOTP.as_view()),
-    path("verify-otp/",views.VerifyOTP.as_view()),
+    path("notevalue/",views.NoteValueView.as_view()),
+    path("get-otp/",views.SendOTPView.as_view()),
+    path("verify-otp/",views.VerifyOTPView.as_view()),
 ]
